79dd31c48ec8cbc2dfc28d78d5e77e61b65445f8
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2022-12-07 01:02:07 +0200

    reformat urls.py

--- donate_ukraine/urls.py
+++ donate_ukraine/urls.py
@@ -12,14 +12,12 @@ DETAILS_VIEWSET_MAPPING = {"get": "retrieve", "put": "update"}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # send 'username' and 'password', receive 'access' and 'refresh' tokens
     path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("logout", LogoutViewSet.as_view({"post": "post"})),
-    # send 'refresh', receive 'access'
     path("login/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout", LogoutViewSet.as_view({"post": "post"})),
+    path("users/info", UserInfoViewSet.as_view({"get": "retrieve"})),
     path("lots/", LotViewSet.as_view(LIST_VIEWSET_MAPPING)),
     path("lots/<int:pk>", LotViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
-    path("users/", UserViewSet.as_view(LIST_VIEWSET_MAPPING)),
-    path("users/<int:pk>", UserViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
-    path("users/info", UserInfoViewSet.as_view({"get": "retrieve"})),  # get current user data
+    # path("users/", UserViewSet.as_view(LIST_VIEWSET_MAPPING)),
+    # path("users/<int:pk>", UserViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
 ]
