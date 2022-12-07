cbede5d858d0fffd32c278da6284614cae085ecc
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2022-12-07 04:29:03 +0200

    tests added

---
+++ donate_ukraine/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+# import django_pytest
+
+# from donate_ukraine.models import User
+from django.conf import settings
+import pytest
+
+
+@pytest.mark.django_db
+@pytest.fixture(autouse=True)
+def django_db_setup():
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": "example",
+        "NAME": "test_db",
+        "ATOMIC_REQUESTS": False,
+    }
+
+
+@pytest.fixture
+def admin_user_credentials(admin_user):
+    return {"username": admin_user.username, "password": "password"}
