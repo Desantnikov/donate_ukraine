63abd8d181fdd00514d71187e0595161454ccb99
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2022-12-07 09:08:58 +0200

    make media/image/<pk> endpoint return base64 encoded image

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,6 @@
+from encodings.base64_codec import base64_encode
+from rest_framework.response import Response
+
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.serializers import ModelSerializer
 
@@ -15,9 +18,19 @@ class LotImageSerializer(ModelSerializer):
 class LotImageViewSet(GenericViewSet, ListCreateRetrieveUpdateMixin):
     view_permissions = {
         "list": {"admin": True, "user": True, "auctioneer": True},
-        "create": {"admin": True, "auctioneer": True},
-        "retrieve": {"admin": True, "auctioneer": True},
+        "create": {"admin": True, "user": True, "auctioneer": True},
+        "retrieve": {"admin": True, "user": True, "auctioneer": True},
     }
 
     queryset = LotImage.objects.all()
     serializer_class = LotImageSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        response_data = {
+            "base64_encoded": base64_encode(instance.file.read()),
+            "name": instance.name,
+        }
+
+        return Response(data=response_data)
