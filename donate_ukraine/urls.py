435298624473b43ac55c6057dd87b2bdab4b6b06
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2022-12-07 02:14:28 +0200

    black ignore urls.py, reformatted urls.py

--- donate_ukraine/urls.py
+++ donate_ukraine/urls.py
@@ -12,12 +12,15 @@ DETAILS_VIEWSET_MAPPING = {"get": "retrieve", "put": "update"}
 
 urlpatterns = [
     path("admin", admin.site.urls),
+
     path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("logout", LogoutViewSet.as_view({"post": "post"})),
-    path("users/info", UserInfoViewSet.as_view({"get": "retrieve"})),
-    path("lots/", LotViewSet.as_view(LIST_VIEWSET_MAPPING)),
-    path("lots/<int:pk>", LotViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
+
     path("users", UserViewSet.as_view(LIST_VIEWSET_MAPPING)),
     path("users/<int:pk>", UserViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
+    path("users/info", UserInfoViewSet.as_view({"get": "retrieve"})),
+
+    path("lots/", LotViewSet.as_view(LIST_VIEWSET_MAPPING)),
+    path("lots/<int:pk>", LotViewSet.as_view(DETAILS_VIEWSET_MAPPING)),
 ]
