81e55660fbaebcb820bba418e3d75a60f1fe598a
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-01-12 23:50:12 +0200

    make api-schema url no trailing slash

--- urls.py
+++ urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api-schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-view'),
+    re_path('api-schema', schema_view.with_ui('swagger', cache_timeout=0), name='schema-view'),
     path('admin/', admin.site.urls),
 
     path('', include(router.urls)),
