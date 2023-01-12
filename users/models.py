5842656aa1bef1e16af127e395faf23fe48f9802
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-01-12 23:52:38 +0200

    added forgotten file

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import UserManager as DjangoUserManager, AbstractUser
+from django.db import models
+
+# Create your models here.
+class UserManager(DjangoUserManager):
+    def create_superuser(self, *args, **kwargs):
+        user = super().create_superuser(role="admin", *args, **kwargs)
+
+        return user
+
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ["password", "api_token"]
+
+    objects = UserManager()
+
+    # to fetch data from auction creator's monobank jar
+    api_token = models.CharField(max_length=60, null=False)  # encrypt
+    role = models.CharField(max_length=20, default="user")
+
+    phone_number = models.CharField(max_length=20, default="")  # needed for moderation
---
+++ users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from donate_ukraine.mixins.views import ListCreateRetrieveUpdateMixin
+from users.serializers import UserSerializer
+from users.models import User
+
+
+class UserViewSet(GenericViewSet, ListCreateRetrieveUpdateMixin):  # TODO: remove list all users
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(methods=["get"], detail=False, url_name="info", url_path="info", permission_classes=[IsAuthenticated])
+    def info(self, request):
+        serializer = self.get_serializer_class()(request.user)
+        return Response(serializer.data)
+
+
+class LogoutViewSet(GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args):
+        refresh = RefreshToken.for_user(request.user)
+        refresh.blacklist()
+        return Response("Logged out")
