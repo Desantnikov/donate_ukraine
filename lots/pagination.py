b1a010f461427a3474cca3b6a5e509fdcb3fddb5
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-02-09 15:42:58 +0200

    adjust pagination

---
+++ lots/pagination.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        next_page_url = self.get_next_link()  # "http://localhost:8000/lots?page=2"
+        previous_page_url = self.get_previous_link()
+
+        # TODO: rework this ugly w/a which is used to show just indexes
+        if next_page_url is not None:
+            next_page_url = int(next_page_url[-1])
+
+        if previous_page_url is not None:
+            previous_page_url = int(previous_page_url[-1])
+
+        return Response(
+            {
+                "current_page": self.page.number,
+                "next_page": next_page_url,
+                "previous_page": previous_page_url,
+                "total_pages": self.page.paginator.count,
+                "results": data,
+            }
+        )
