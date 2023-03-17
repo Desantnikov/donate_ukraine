b2823de1218aab20758789d92bce4fb70ba731f8
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-03-17 10:28:37 +0200

    let user delete photos

--- storage/views.py
+++ storage/views.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from mixins.views import ListCreateRetrieveUpdateMixin
+from mixins.views import AllActionsMixin
 from storage.models import LotImage
 from storage.serializers import ImageSerializer
 
 
-class LotImageViewSet(GenericViewSet, ListCreateRetrieveUpdateMixin):
+class LotImageViewSet(GenericViewSet, AllActionsMixin):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = ImageSerializer
