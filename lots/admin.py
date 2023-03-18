4de66d0038856f6d33e35ad32542ed7e707378c2
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-03-18 20:12:27 +0200

    add winner picking type to admin

--- lots/admin.py
+++ lots/admin.py
@@ -6,7 +6,17 @@ from lots.models import Lot
 @admin.register(Lot)
 class LotAdmin(admin.ModelAdmin):
     list_display = ("name", "creator", "status", "created_at")
-    fields = ("creator", "name", "description", "ending_date", "monobank_jar", "status", "created_at", "deleted_at")
+    fields = (
+        "creator",
+        "name",
+        "description",
+        "ending_date",
+        "monobank_jar",
+        "status",
+        "winner_picking_type",
+        "created_at",
+        "deleted_at",
+    )
     readonly_fields = ("created_at", "deleted_at")
 
     def get_queryset(self, request):
