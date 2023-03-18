3296294b57c52f046c64b4dbf46c66d9391745d5
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-03-18 04:23:10 +0200

    lot statuses name value swap back

--- lots/constants.py
+++ lots/constants.py
@@ -2,11 +2,10 @@ from enum import Enum
 
 
 class LOT_STATUS(str, Enum):
-    MODERATION = "Under moderation"  # not shown to user
-    ACTIVE = "Active"  # active lots
-    CLOSED = "Closed"  # finished lots
+    MODERATION = "MODERATION"  # not shown to user
+    ACTIVE = "ACTIVE"  # active lots
+    CLOSED = "CLOSED"  # finished lots
 
     @classmethod
     def values(cls):
-        # [("MODERATION", "Under moderation"), ("ACTIVE", "Active"), ...]
-        return [(i.value, i.name) for i in cls]
+        return [(i.name, i.value) for i in cls]
