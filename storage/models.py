8a45d9d77d792bfa742934e709ca25d08ce026ad
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-03-21 04:19:55 +0200

    store different filetypes for lot

--- storage/models.py
+++ storage/models.py
@@ -4,6 +4,7 @@ from lots.models import Lot
 
 
 class LotImage(models.Model):
-    file = models.ImageField(upload_to="images/lots")
+    # file = models.ImageField(upload_to="images/lots")
+    file = models.FileField(upload_to="images/lots")  # FileField instead of ImageField to also accept videos and stuff
 
     lot_id = models.ForeignKey(Lot, models.CASCADE)
