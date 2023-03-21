b97a9e0e10f05c072c09d9429cbe6ec780cd5a70
Anton Desiatnykov <anton.desyiatnikov@sendcloud.com>
2023-03-21 04:48:21 +0200

    file serializer show filetype

--- storage/serializers.py
+++ storage/serializers.py
@@ -1,13 +1,23 @@
+import mimetypes
+
 from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from storage.models import LotImage
 
 
 class ImageSerializer(ModelSerializer):
+    file_type = SerializerMethodField()
+
     class Meta:
         model = LotImage
         fields = "__all__"
+
+    def get_file_type(self, obj):
+        try:
+            return mimetypes.guess_type(obj.file.name)[0]
+        except:
+            return "unknown"
 
     def validate(self, attrs):
         user = self.context["request"].user
